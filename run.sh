0c72f6ae7008c1905727c8add4e15eb69f2c07a0
kopardev <vishal.koparde@nih.gov>
2021-07-28 15:05:47 -0400

    run.sh updated with WORKDIR etc.

--- run.sh
+++ run.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 WORKDIR=$1
+# runmode = cluster or dryrun or unlock
 RUNMODE=$2
-if [ ! -d $WORKDIR ]; then mkdir -p $WORKDIR;fi
-cat << EOF > submit.sh 
-#!/bin/bash
-#SBATCH --cpus-per-task=4 
-#SBATCH --mem=8g
-#SBATCH --time=1-00:00:00 
-#SBATCH --parsable 
-#SBATCH -J "Nanoseq" 
-#SBATCH --mail-type=BEGIN,END,FAIL
-module load snakemake
-module load singularity
-snakemake \
---directory $WORKDIR \
---use-singularity \
---singularity-args "'-B $WORKDIR'" \
---use-envmodules \
---printshellcmds \
---latency-wait 120 \
---cluster-config resources/cluster.json \
---cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --job-name {cluster.name} --output {cluster.output} --error {cluster.error} " \
--j 500 \
---rerun-incomplete \
---keep-going \
---stats "${WORKDIR}/snakemake.stats" \
-2>&1 | tee "${WORKDIR}/snakemake.log"
-EOF
 
-sbatch submit.sh
+PIPELINE_HOME="/data/millerv2/NanoseqSnakemake"
+SNAKEFILE="$PIPELINE_HOME/workflow/Snakefile"
+
+if [ "$RUNMODE" == "dryrun" ];then
+	snakemake -n \
+	-s $SNAKEFILE \
+	--directory $WORKDIR \
+	--printshellcmds \
+	--cluster-config $PIPELINE_HOME/resources/cluster.json \
+	--cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --job-name {cluster.name} --output {cluster.output} --error {cluster.error} " \
+	-j 500
+elif [ "$RUNMODE" == "unlock" ];then
+	snakemake --unlock \
+	-s $SNAKEFILE \
+	--directory $WORKDIR
+elif [ "$RUNMODE" == "cluster" ];then
+	if [ ! -d $WORKDIR ]; then mkdir -p $WORKDIR;fi
+	cat << EOF > submit.sh 
+	#!/bin/bash
+	#SBATCH --cpus-per-task=4 
+	#SBATCH --mem=8g
+	#SBATCH --time=1-00:00:00 
+	#SBATCH --parsable 
+	#SBATCH -J "Nanoseq" 
+	#SBATCH --mail-type=BEGIN,END,FAIL
+	module load snakemake
+	module load singularity
+	snakemake \
+	-s $SNAKEFILE \
+	--directory $WORKDIR \
+	--use-singularity \
+	--singularity-args "'-B $WORKDIR'" \
+	--use-envmodules \
+	--printshellcmds \
+	--latency-wait 120 \
+	--cluster-config $PIPELINE_HOME/resources/cluster.json \
+	--cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --job-name {cluster.name} --output {cluster.output} --error {cluster.error} " \
+	-j 500 \
+	--rerun-incomplete \
+	--keep-going \
+	--stats "${WORKDIR}/snakemake.stats" \
+	2>&1 | tee "${WORKDIR}/snakemake.log"
+	EOF
+
+	sbatch submit.sh
+fi
